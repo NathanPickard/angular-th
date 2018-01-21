00f995a626f9b80195527be7bd7b998b2bbbc037
Nathan Pickard <nathan.pickard87@gmail.com>
2018-01-20 16:00:47 -0800

    component templates and structural directives

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['entry.component.css']
 })
 
-export class EntryComponent { }
+export class EntryComponent {
+  title: string = 'My First Photo';
+  photo: string = 'http://placehold.it/800x500?text=Angular Basics';
+  description: string = 'A Description of My First Photo';
+  comments: any[] = [
+    {name: "John", comment: "A comment"},
+    {name: "Jim", comment: "A comment"},
+    {name: "Jen", comment: "A comment"}
+  ]
+ }
